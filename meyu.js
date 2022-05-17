ff19924160637fb63c526bff1f7725548d77d1be
Imejuart1 <101446446+Imejuart1@users.noreply.github.com>
2022-05-17 12:59:39 +0100

    RESPONSIVE WEB, NAVIGATION AND FORM

---
+++ meyu.js
@@ -0,0 +1,32 @@
+function validatePassword(){
+    if(password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+      confirm_password.setCustomValidity('');
+    }
+  }
+  
+  password.onchange = validatePassword;
+  confirm_password.onkeyup = validatePassword;
+  
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  const togglePassword = document.querySelector('#togglePassword');
+  const rassword = document.querySelector('#password');
+  
+ 
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = rassword.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+
